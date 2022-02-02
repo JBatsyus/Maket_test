cad7bfd1a52ea61662d4c45c8fd387dac2b9b012
Yuliya Batsyus <ju.kalabina@gmail.com>
2022-02-02 17:31:38 +0200

    fix: router

--- src/App.js
+++ src/App.js
@@ -16,6 +16,9 @@ function App() {
           <Route exact path="/user">
             <User />
           </Route>
+          <Route exact path="/">
+            <Main />
+          </Route>
         </Switch>
       </div>
     </Router>
